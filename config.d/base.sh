835d10a4d63fe51ded709b42a587295c5e42564b
Masahito Yoshida <masahito@axsh.net>
2014-11-21 18:38:00 +0900

    install ifup-macvlan/ifdown-macvlan scripts.

--- config.d/base.sh
+++ config.d/base.sh
@@ -8,3 +8,8 @@ set -o pipefail
 set -x
 
 # Do some changes ...
+
+for i in ifdown-macvlan ifup-macvlan; do
+  curl -fsSkL https://raw.githubusercontent.com/larsks/initscripts-macvlan/master/ifdown-macvlan -o /etc/sysconfig/network-scripts/${i}
+  chmod +x /etc/sysconfig/network-scripts/${i}
+done
